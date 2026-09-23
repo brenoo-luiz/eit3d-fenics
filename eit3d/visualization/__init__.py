06819039d9b9beeb98eb5e9825a711a25e7f0b8c
Breno Luiz <breno.snr@gmail.com>
2026-09-23 18:17:47 -0300

    feat(visualization): export SurfacePanel

--- eit3d/visualization/__init__.py
+++ eit3d/visualization/__init__.py
@@ -1,5 +1,5 @@
 from eit3d.visualization.base import BaseRenderer
-from eit3d.visualization.static import StaticRenderer
+from eit3d.visualization.static import StaticRenderer, SurfacePanel
 from eit3d.visualization.interactive import InteractiveRenderer
 
-__all__ = ["BaseRenderer", "StaticRenderer", "InteractiveRenderer"]
+__all__ = ["BaseRenderer", "StaticRenderer", "SurfacePanel", "InteractiveRenderer"]
